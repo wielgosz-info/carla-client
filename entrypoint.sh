2089d03c189c377aa5432e254de04c443227d85a
Maciej Wielgosz <wielgosz@agh.edu.pl>
2022-01-30 12:11:40 +0100

    Minimal container with CARLA client

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /app
+
+if [ -z "${PACKAGE}" ]; then
+    echo "PACKAGE is not set or blank, skipping installation."
+else
+    echo "Installing ${PACKAGE}@${COMMIT}..."
+    SETUPTOOLS_SCM_PRETEND_VERSION="0.0.post0.dev38+${COMMIT}.dirty" /venv/bin/python -m pip install --no-cache-dir -e /app
+fi
+
+echo "Executing CMD"
+exec "$@"
